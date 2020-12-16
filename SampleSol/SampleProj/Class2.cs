c9b6fc9ece88ce7e2085de8a59ac3883120447ee
Adityarajjain <76032670+Adityarajjain@users.noreply.github.com>
2020-12-16 10:44:25 +0530

    Create Class2.cs
    Class 2 added

---
+++ SampleSol/SampleProj/Class2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProj
+{
+    partial class Program
+    {
+        void printWelcome()
+        {
+            Console.WriteLine("Welcome from class 2");
+        }
+    }
+}
